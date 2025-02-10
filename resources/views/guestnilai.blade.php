aee238e2b1722445b318c56691c030b41877bd5f
Adri Syahrul <adrisyahrul20@gmail.com>
2025-02-10 08:08:59 +0700

    fix minor

--- resources/views/guestnilai.blade.php
+++ resources/views/guestnilai.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Daftar Nilai Siswa</title>
+    <title>Monitor Nilai Siswa SMA N 2 Bayang</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
