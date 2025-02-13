e78324216d19a23b02e235d4b10e4e9c72d0b440
Adri Syahrul <adrisyahrul20@gmail.com>
2025-02-13 19:59:16 +0700

    fix minor

--- resources/views/administrator/dashboard/index.blade.php
+++ resources/views/administrator/dashboard/index.blade.php
@@ -64,14 +64,14 @@
                     data: {
                         labels: labels,
                         datasets: [{
-                                label: 'Lulus',
+                                label: 'Tuntas',
                                 data: lulusData,
                                 backgroundColor: 'rgba(75, 192, 192, 0.6)',
                                 borderColor: 'rgba(75, 192, 192, 1)',
                                 borderWidth: 1
                             },
                             {
-                                label: 'Tidak Lulus',
+                                label: 'Tidak Tuntas',
                                 data: tidakLulusData,
                                 backgroundColor: 'rgba(255, 99, 132, 0.6)',
                                 borderColor: 'rgba(255, 99, 132, 1)',
